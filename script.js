a1d1b09ab425741e15620a4cdc25a60757ecc4d1
Volantis <putzstuck@gmail.com>
2022-10-11 17:15:47 -0400

    added local storage functionality for all buttons

--- script.js
+++ script.js
@@ -1,28 +1,80 @@
-var task = [];
+// var task = [];
 
 $(document).ready(function(){
 
 function init(){
-    var storedTask = JSON.parse(window.localStorage.getItem('task'));
-    if (storedTask!== null) {
-        task = storedTask;
-      }
+    var storedTask = localStorage.getItem('task9am');
+    $('#task9am').text(storedTask);
+    storedTask = localStorage.getItem('task10am');
+    $('#task10am').text(storedTask);
+    storedTask = localStorage.getItem('task11am');
+    $('#task11am').text(storedTask);
+    storedTask = localStorage.getItem('task12pm');
+    $('#task12pm').text(storedTask);
+    storedTask = localStorage.getItem('task1pm');
+    $('#task1pm').text(storedTask);
+    storedTask = localStorage.getItem('task2pm');
+    $('#task2pm').text(storedTask);
+    storedTask = localStorage.getItem('task3pm');
+    $('#task3pm').text(storedTask);
+    storedTask = localStorage.getItem('task4pm');
+    $('#task4pm').text(storedTask);
+    storedTask = localStorage.getItem('task5pm');
+    $('#task5pm').text(storedTask);
 }
 
-function storeTasks(){
-    localStorage.setItem("task", JSON.stringify(task));
-}
+$('#btn9am').click(function(){
+    var text = $('#task9am').val();
+    alert("9:00 AM Task Saved!");
+    localStorage.setItem("task9am", text);
+});
 
-$('#saveBtn').click(function(){
-    var text = $('#task').val();
-    if (text === ""){
-        return;
-    }
-    task.push(text);
-    // text.value = "";
-    console.log(text);
-    alert("Task Saved!");
-    storeTasks();
+$('#btn10am').click(function(){
+    var text = $('#task10am').val();
+    alert("10:00 AM Task Saved!");
+    localStorage.setItem("task10am", text);
+});
+
+$('#btn11am').click(function(){
+    var text = $('#task11am').val();
+    alert("11:00 AM Task Saved!");
+    localStorage.setItem("task11am", text);
+});
+
+$('#btn12pm').click(function(){
+    var text = $('#task12pm').val();
+    alert("12:00 PM Task Saved!");
+    localStorage.setItem("task12pm", text);
+});
+
+$('#btn1pm').click(function(){
+    var text = $('#task1pm').val();
+    alert("1:00 PM Task Saved!");
+    localStorage.setItem("task1pm", text);
+});
+
+$('#btn2pm').click(function(){
+    var text = $('#task2pm').val();
+    alert("2:00 PM Task Saved!");
+    localStorage.setItem("task2pm", text);
+});
+
+$('#btn3pm').click(function(){
+    var text = $('#task3pm').val();
+    alert("3:00 PM Task Saved!");
+    localStorage.setItem("task3pm", text);
+});
+
+$('#btn4pm').click(function(){
+    var text = $('#task4pm').val();
+    alert("4:00 PM Task Saved!");
+    localStorage.setItem("task4pm", text);
+});
+
+$('#btn5pm').click(function(){
+    var text = $('#task5pm').val();
+    alert("What are you doing?! It's 5 o'clock!");
+    localStorage.setItem("task5pm", text);
 });
 
 init();
