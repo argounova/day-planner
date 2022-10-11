3c4937d575344f70f4d7c257458265792c98ce56
Volantis <putzstuck@gmail.com>
2022-10-11 09:51:12 -0400

    working through local storage issues on script file

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
+var task = [];
+
 $(document).ready(function(){
 
-$('#btn9').click(function(){
-    var text = $('#event9').val();
-    localStorage.setItem("Event", JSON.stringify(text));
+function init(){
+    var storedTask = JSON.parse(window.localStorage.getItem('task'));
+    if (storedTask!== null) {
+        task = storedTask;
+      }
+}
+
+function storeTasks(){
+    localStorage.setItem("task", JSON.stringify(task));
+}
+
+$('#saveBtn').click(function(){
+    var text = $('#task').val();
+    if (text === ""){
+        return;
+    }
+    task.push(text);
+    // text.value = "";
     console.log(text);
-    alert("Event Saved!");
+    alert("Task Saved!");
+    storeTasks();
 });
 
+init();
+
 });
+
